abcf2a760d4cd6f93dba686b42680e1c1d8a60d1
MahedyHHasan17 <mhdyhasan017@gmail.com>
2025-05-16 09:53:14 +0600

    adding testing folder to each module

---
+++ modules/Order/Tests/OrderTest.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace Modules\Order\Tests ;
+
+use Tests\TestCase;
+
+class OrderTest extends TestCase{
+
+
+      public function test_order_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+
+}
---
+++ modules/Product/Tests/ProductTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Product\Tests;
+
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function test_product_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}

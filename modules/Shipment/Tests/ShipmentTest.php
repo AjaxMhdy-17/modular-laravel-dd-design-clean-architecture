f03ae8b25edd36c92b2f9f7d331e3fb487420aa4
MahedyHHasan17 <mhdyhasan017@gmail.com>
2025-05-16 09:55:29 +0600

    adding testing folder to each module completed

---
+++ modules/Shipment/Tests/ShipmentTest.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Modules\Shipment\Tests ;
+
+use Tests\TestCase;
+
+Class ShipmentTest extends TestCase{
+    public function test_shipment_ok(){
+        $this->assertTrue(True) ; 
+    }
+}
